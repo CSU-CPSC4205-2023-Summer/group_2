0a80b1fa5c2ac355c4e913b4303afa6c1d63cd34
Sirblufu <123429403+Sirblufu@users.noreply.github.com>
2023-07-08 15:13:06 -0400

    Add files via upload
    API Caller to Spotify API.  Current iteration simply pull a search of tracks from a specific year and pulls song titles, artist names, internal popularity scores and track ID's and prints them out in a massive matrix. Requires 'pip install spotipy' into your Command Line before calling spotipy library.  Final iterations will utilize already acquired ID's for authentication.

---
+++ API Caller.py
@@ -0,0 +1,20 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import requests
+import json
+cid = 'Your Client ID'
+secret = 'Your Secret ID'
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+artist_name = []
+track_name = []
+popularity = []
+track_id = []
+for i in range(0,950,50):
+    track_results = sp.search(q='year:2018', type='track', limit=50,offset=i)
+    for i, t in enumerate(track_results['tracks']['items']):
+        artist_name.append(t['artists'][0]['name'])
+        track_name.append(t['name'])
+        track_id.append(t['id'])
+        popularity.append(t['popularity'])
+print(artist_name, track_name, track_id, popularity)
