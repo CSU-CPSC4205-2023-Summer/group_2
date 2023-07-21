3a38f39ec275e46b3437b5d3472c41fb05d1b600
Sirblufu <123429403+Sirblufu@users.noreply.github.com>
2023-07-21 16:11:12 -0400

    Update app.py
    Final Update w/ cover art and preview functionality to listen to samples of songs in search when available

--- app.py
+++ app.py
@@ -1,26 +1,22 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, Response
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from operator import itemgetter
+import requests
 
 app = Flask(__name__)
 
+# Replace with your Spotify API credentials
 cid = 'ecb42b03924a473eac366b5d16f2b65f'
 secret = 'fbddb37fb3e843bea5b87ed537ab0e7c'
+
+# Initialize the Spotify client
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 @app.route("/")
 def home():
     return render_template("firstpage.html")
-
-@app.route("/plans")
-def plans1():
-    return render_template("secondpage.html")
-
-@app.route("/library")
-def library1():
-    return render_template("thirdpage.html")
 
 @app.route('/TopSongs', methods=['GET', 'POST'])
 def get_top_songs():
@@ -31,34 +27,59 @@ def get_top_songs():
         search_option = 'artist'
         search_query = ''
 
-    artist_name = []
     track_name = []
-    popularity = []
-    track_id = []
+    artist_name = []
+    cover_url = []
+    audio_preview_url = []
 
     if search_option == 'artist':
         track_results = sp.search(q=f'artist:{search_query}', type='track', limit=50)
     else:
         track_results = sp.search(q=f'year:{search_query}', type='track', limit=50)
 
-    for i, t in enumerate(track_results['tracks']['items']):
-        artist_name.append(t['artists'][0]['name'])
+    for t in track_results['tracks']['items']:
         track_name.append(t['name'])
-        track_id.append(t['id'])
-        popularity.append(t['popularity'])
+        artist_name.append(t['artists'][0]['name'])
+        cover_url.append(t['album']['images'][0]['url'])
+        audio_preview_url.append(t['preview_url'])
 
     # Create a list of (popularity, track) tuples
-    tracks_with_popularity = list(zip(popularity, zip(track_name, artist_name)))
+    tracks_with_popularity = list(zip(range(len(track_results['tracks']['items'])), zip(track_name, artist_name, cover_url, audio_preview_url)))
 
     # Sort tracks by popularity in descending order
     sorted_tracks = sorted(tracks_with_popularity, key=itemgetter(0), reverse=True)
 
     # Generate HTML for the top 10 songs by popularity
     top_songs_html = ''
-    for i, (popularity, (track, artist)) in enumerate(sorted_tracks[:10]):
-        top_songs_html += f"<p>{i+1}. {track} by {artist}</p>"
+    for i, (popularity, (track, artist, cover_url, audio_preview_url)) in enumerate(sorted_tracks[:10]):
+        if audio_preview_url is not None:
+            top_songs_html += f"""
+                <p>{i+1}. {track} by {artist}</p>
+                <img src="{cover_url}" alt="Cover Art" width="200" height="200">
+                <button class="play-button">Play</button>
+                <audio>
+                    <source src="/proxy_audio?url={audio_preview_url}" type="audio/mpeg">
+                    Your browser does not support the audio element.
+                </audio>
+            """
+        else:
+            top_songs_html += f"""
+                <p>{i+1}. {track} by {artist}</p>
+                <img src="{cover_url}" alt="Cover Art" width="200" height="200">
+                <p>No audio preview available</p>
+            """
 
     return render_template('index.html', search_option=search_option, search_query=search_query, top_songs=top_songs_html)
+
+@app.route('/proxy_audio', methods=['GET'])
+def proxy_audio():
+    audio_url = request.args.get('url')
+    if not audio_url:
+        return Response(status=400)
+
+    # Fetch the audio data from Spotify and serve it as a response
+    audio_data = requests.get(audio_url).content
+    return Response(audio_data, content_type='audio/mpeg')
 
 if __name__ == "__main__":
     app.run()
